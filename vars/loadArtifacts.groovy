19e8ae02a5f4f3786b937e7b5e3fbec146d41e90
Adrián P <adrian.pino@i2cat.net>
2023-02-22 11:33:15 +0100

    Deleting unused scripts and renaming loadArtifact.groovy

--- vars/loadArtifacts.groovy
+++ vars/loadArtifacts.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
     loadLinuxScript(name: 'load_artifacts.sh')
-    sh "./load_artifacts.sh ${config.GitRepo} ${config.File}"
+    sh "./load_artifacts.sh ${config.GitRepository} ${config.File}"
 }
--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    def scriptcontents = libraryResource "com/gap/scripts/${config.name}"
+    def scriptcontents = libraryResource "scripts/${config.name}"
     writeFile file: "${config.name}", text: scriptcontents
     sh "chmod a+x ./${config.name}"
 }
