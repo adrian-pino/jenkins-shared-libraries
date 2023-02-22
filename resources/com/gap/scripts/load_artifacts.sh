7104a7fc52b1b83874eb63ab90b97bcdc42ca93d
Adrián P <adrian.pino@i2cat.net>
2023-02-22 09:50:10 +0100

    Testing load_artifact script from shared libraries

--- resources/com/gap/scripts/load_artifacts.sh
+++ resources/com/gap/scripts/load_artifacts.sh
@@ -7,15 +7,15 @@ mkdir ~/tmp-folder
 cd ~/tmp-folder
 
 # Pull the git repository
-git clone git@github.com:adrian-pino/jenkins-common-files.git
+git clone $1
 cd jenkins-common-files
 
-# Add new artifacts into the repository
-cp ./config/config.cfg $CURRENT_PATH 
+# Load the artifact from the Git repo folder
+cp ./config/$2 $CURRENT_PATH 
 cd $CURRENT_PATH
 
 # Validate that the config file got loaded
-cat config.cfg
+echo $2
 
 # Clean everything
 rm -rf ~/tmp-folder
